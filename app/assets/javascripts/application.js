69e3bc32062bdda5aad1926591449e7a459d369c
James Medd <james.medd@sparck.io>
2024-06-18 14:33:42 +0100

    Change notifications to use plugin (#66)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,6 +59,3 @@ window.GOVUKPrototypeKit.documentReady(() => {
 
 })
 
-const platform = navigator.userAgent.includes('Macintosh') ? 'mac' : navigator.userAgent.includes('Windows') ? 'win' : 'other'
-
-document.body.classList.add(`notification-${platform}`)
